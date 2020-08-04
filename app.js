0aaeef4b8bd7fcbe8fbc924a2b7baad3b6128f66
Luiz Flávio Clink <lfclink@gmail>
2020-08-04 18:05:59 -0300

    Ajustes do Cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express       = require('express')
 const bodyParser    = require('body-parser')
+const cors          = require('cors')
 
 const routes        = require('./src/routes')
 
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json())
-
+app.use(cors())
 app.use(routes)
 app.use(express.json())
 
